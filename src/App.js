b537dbf5e27e7a6409ed0ceecc405bb5c0e7f5d6
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-13 17:15:28 -0400

    add redux thunk

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
 
+import reducers from './reducers';
 import { Header } from './components/common';
 
-const store = createStore();
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 class App extends Component {
 
