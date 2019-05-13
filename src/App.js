fd4d8027e1ea471e0c1150e861f566e5b884123e
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-13 17:44:37 -0400

    add redux thunk

--- src/App.js
+++ src/App.js
@@ -11,6 +11,19 @@ const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 class App extends Component {
 
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAhBiZ4d3vmSrVGLKzlTRwfNMNW6HzeFZQ',
+      authDomain: 'fskeeper-44ef4.firebaseapp.com',
+      databaseURL: 'https://fskeeper-44ef4.firebaseio.com',
+      projectId: 'fskeeper-44ef4',
+      storageBucket: 'fskeeper-44ef4.appspot.com',
+      messagingSenderId: '530968158926',
+      appId: '1:530968158926:web:7fee044c77aa6513'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <Provider store={store} >
