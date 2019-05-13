1d8b851f78fa89c6a848e573ace224dac991639e
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-05-13 18:13:31 -0400

    add Router

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
+import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
   return (
